48e3f17e3a0bc1d41532adea41d5d65bfb0b73bc
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-03-29 06:52:51 -0500

    Development
    Rolled back the before action as it was not needed for this application at the level I was setting it at.
    
    footer
    Changes to be committed:
      modified:   app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,6 @@ class ApplicationController < ActionController::Base
 
   # Allowing custom parameters for Devise
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # Checks to make sure a user is logged in, this will also redirect with expired sessions that have ended.
-  before_action :is_signed_in?
-  # Have to skip the is_signed_in? method with authentication pages because that is where user signs in.
-  skip_before_action :is_signed_in?, if: :devise_controller?
-
-  private
-    # redirect if no user is detected
-    def is_signed_in?
-      unless user_signed_in? 
-        redirect_to new_user_session_path
-      end
-    end
 
   protected
 
