a95944aacaa52612b89bdd9b482b552c06718cee
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-03-30 11:03:48 -0500

    Development
    Added a navbar and styled that along with tweaking the posts and auth pages.
    
    Changes to be committed:
      modified:   Gemfile
      modified:   Gemfile.lock
      modified:   README.rdoc
      modified:   app/assets/stylesheets/authentication.scss
      modified:   app/assets/stylesheets/posts.scss
      modified:   app/assets/stylesheets/site.scss
      modified:   app/views/layouts/_navbar.html.erb
      modified:   db/seeds.rb

--- Gemfile
+++ Gemfile
@@ -61,9 +61,10 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  # Generate fake data for local testing
+  gem 'faker'
 end
 
 group :development, :production do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Generate some fake users
+# 5.times do
+#   id = rand(1..500)
+#   first_name = Faker::Superhero.prefix
+#   last_name = Faker::Superhero.name
+#   email = "#{first_name}_the_great_#{id}@user.com"
+#   password = "password"
+#   User.create!(id: id, first_name: first_name, last_name: last_name, email: email, password: password, password_confirmation: password, confirmed_at: Time.now)
+# end
+
+# Generate some fake posts
+# 25.times do
+#   id = rand(1..350)
+#   title = Faker::Book.title
+#   body = Faker::ChuckNorris.fact
+#   user = User.first
+#   Post.create!(id: id, title: title, body: body, user_id: user.id)
+# end
