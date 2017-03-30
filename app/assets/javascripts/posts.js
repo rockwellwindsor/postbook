5549723b501177b76b514bc5f5473615591bc85a
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-03-30 12:42:34 -0500

    Development
    returning success of AJAX comment create.
    
    Changes to be committed:
    	modified:   app/assets/javascripts/posts.js
    	modified:   app/assets/stylesheets/comments.scss
    	modified:   app/assets/stylesheets/posts.scss
    	modified:   app/controllers/comments_controller.rb
    	modified:   app/views/comments/_form.html.erb
    	modified:   app/views/posts/show.html.erb

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,7 +1,21 @@
 $(document).on('click', '.add-comment-button', function() {
-    var post = $(this).attr('data-post-id');
-    $(this).fadeOut(50);
-    $('#new-comment-form-for-post-' + post).load('/comments/new', function() {
-        $('.new-comment-form').fadeIn(500);
-    });
+  var post = $(this).attr('data-post-id');
+  $(this).fadeOut(50);
+  $('#new-comment-form-for-post-' + post).load('/comments/new', function() {
+    $('.new-comment-form').fadeIn(500);
+  });
+});
+
+$(document).on('click', '.create-comment-button', function() {
+  var post = $('.add-comment-button').attr('data-post-id');
+  var body = document.getElementById("comment_body").value
+  $.ajax({
+    type: "POST",
+    url: '/comments/',
+    dataType: 'script',
+    data: { comment : {body: body, post_id : post}},
+    success: function() {
+      alert('Boom');
+    }
+  });
 });
