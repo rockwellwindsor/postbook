bf8fb4eef5aa355fb2c653f96e93984171fc6337
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-03-31 12:38:32 -0500

    Fix
    Imported ckeditor files needed for production.
    
    Changes to be committed:
    	modified:   app/assets/.DS_Store
    	new file:   app/assets/javascripts/.DS_Store
    	new file:   app/assets/javascripts/channels/.keep
    	new file:   app/assets/javascripts/ckeditor/.DS_Store
    	new file:   app/assets/javascripts/ckeditor/basepath.js.erb
    	new file:   app/assets/javascripts/ckeditor/config.js
    	new file:   app/assets/javascripts/ckeditor/plugins/.DS_Store
    	new file:   app/assets/javascripts/ckeditor/plugins/insertpre/.DS_Store
    	new file:   app/assets/javascripts/ckeditor/plugins/insertpre/README.md
    	new file:   app/assets/javascripts/ckeditor/plugins/insertpre/icons/insertpre-color.png
    	new file:   app/assets/javascripts/ckeditor/plugins/insertpre/icons/insertpre.png
    	new file:   app/assets/javascripts/ckeditor/plugins/insertpre/plugin.js
    	new file:   app/assets/javascripts/js/.DS_Store
    	renamed:    app/assets/javascripts/comment.js -> app/assets/javascripts/js/comment.js
    	renamed:    app/assets/javascripts/posts.js -> app/assets/javascripts/js/posts.js
    	renamed:    app/assets/javascripts/site.js -> app/assets/javascripts/js/site.js
    	new file:   app/assets/javascripts/lang/en.js
    	new file:   app/assets/javascripts/lang/pl.js
    	new file:   public/.DS_Store

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,36 @@
+CKEDITOR.editorConfig = function(config) {
+    config.language = 'en';
+    // config.width = '700';
+    config.height = '350';
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+    config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+    config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+    config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+    config.extraPlugins = 'insertpre';
+    config.toolbar = 'Pure';
+    config.allowedContent = true;
+
+    config.toolbar_Pure = [
+        { name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+        { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
+        { name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 
+            'HiddenField' ] },
+        '/',
+        { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+        '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+        { name: 'links', items : [ 'Link','Unlink','Anchor', 'InsertPre' ] },
+        { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+        '/',
+        { name: 'styles', items : [ 'Styles','Format','Font','FontSize','insertpre' ] },
+        { name: 'colors', items : [ 'TextColor','BGColor' ] },
+        { name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
+    ];
+
+  
+    return true;
+     
+}
