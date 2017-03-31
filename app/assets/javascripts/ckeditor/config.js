f89064a63ee2b7d69254524a5afd8b4d639be6f8
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-03-31 17:29:40 -0500

    Development
    Added image buttons in to begin adding in Image uplaods.
    
    Changes to be committed:
      modified:   app/assets/javascripts/ckeditor/config.js

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,7 +2,6 @@ CKEDITOR.editorConfig = function(config) {
     config.language = 'en';
     // config.width = '700';
     config.height = '350';
-    config.enterMode = CKEDITOR.ENTER_BR;
     config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
     config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
     config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
@@ -16,14 +15,14 @@ CKEDITOR.editorConfig = function(config) {
         { name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
         { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
         { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
-        { name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 
+        { name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 
             'HiddenField' ] },
         '/',
         { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
         { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
         '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
         { name: 'links', items : [ 'Link','Unlink','Anchor', 'InsertPre' ] },
-        { name: 'insert', items : [ 'Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+        { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
         '/',
         { name: 'styles', items : [ 'Styles','Format','Font','FontSize','insertpre' ] },
         { name: 'colors', items : [ 'TextColor','BGColor' ] },
@@ -33,4 +32,5 @@ CKEDITOR.editorConfig = function(config) {
   
     return true;
      
+}
 }
