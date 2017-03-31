99e806e150122faef546129cd089c82b6b519e13
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-03-31 10:48:30 -0500

    seed file fix, still getting duplicates, doubting everything at this point

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # # Generate some fake users
-35.times do
-  id = rand(1...350)
-  first_name = Faker::Superhero.prefix
-  last_name = Faker::Superhero.name
-  email = "#{first_name}_the_great#{id}@#{first_name}.com"
-  password = "password"
-  User.create!(first_name: first_name, last_name: last_name, email: email, password: password, password_confirmation: password, confirmed_at: Time.now)
-end
+# 35.times do
+#   id = rand(1...350)
+#   first_name = Faker::Superhero.prefix
+#   last_name = Faker::Superhero.name
+#   email = "#{first_name}_the_great#{id}@#{first_name}.com"
+#   password = "password"
+#   User.create!(first_name: first_name, last_name: last_name, email: email, password: password, password_confirmation: password, confirmed_at: Time.now)
+# end
 
 # Generate some fake posts
 75.times do 
